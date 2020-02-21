3e639a2c110312b16b27bb7287c6ae2fb489eb07
Nore5515 <Nore5515@gmail.com>
2020-02-21 12:44:10 -0600

    testing once more, with comments.

--- tensortest.py
+++ tensortest.py
@@ -1,82 +1,78 @@
-#from __future__ import absolute_import, division, print_function, unicode_literals
+# Also, for future reference, in Atom use command+comment slash to comment lines.
 
-# TensorFlow and tf.keras
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
 import tensorflow as tf
 
-
-from tensorflow import keras
-
-# Helper libraries
-import numpy as np
-#import matplotlib.pyplot as plt
-
-import face_recognition
-
-#from easyfacenet.simple import facenet
+import matplotlib.pyplot as plt
 
 
+mnist = tf.keras.datasets.mnist
 
-print(tf.__version__)
+# x_train and y_train are a list of 60,000 uh...things.
+# x_test and y_test are a list of 10,000...things.
+# OH! I think they're images! I'll check using the image reader.
+# confirm; they are hand drawn numbers.
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-"""
 
-#fashion_mnist = keras.datasets.fashion_mnist
+# print(len(x_train))
+# print(len(y_train))
+# print(len(x_test))
+# print(len(y_test))
 
-#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# dividing them doesn't seem to change much.
+# I'm not sure why we're doing this.
+# and why only the x ones???
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# I see! The Y tuples/arrays is a list of numbers!
+# It's what each image corresponds to!
+#print (y_train)
 
-*train_images.shape
-len(train_labels)
-train_labels
-test_images.shape
-len(test_labels)
+# OOOOOOH WE'RE DIVIDING EVERY VALUE IN THE ARRAYS/TUPLES BY 255
+# THAT WAY THEIR RBG VALUE OF EACH TILE IS ONLY BETWEEN 1 AND 0
 
+# print (x_train[0])
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# print (x_train[0])
+
+# print(len(x_train))
+# print(len(y_train))
+# print(len(x_test))
+# print(len(y_test))
+
+# huh?
+# I believe this generates an empty "figure" state
 plt.figure()
-plt.imshow(train_images[0])
+# shows an image? but not really? from a numpy array?
+# Takes in some Numpy array of (M, N, 3)
+# M is all the pixels, N is each indivdual pixel and 3 is the RGB values
+plt.imshow(x_train[0])
+# presents a colorbar besides the image
 plt.colorbar()
+# disables the overlay grid
 plt.grid(False)
+# show the thing!
 #plt.show()
 
-train_images = train_images / 255.0
-
-test_images = test_images / 255.0
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-#plt.show()
-
-
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
+"""
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
 ])
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10)
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
-print('\nTest accuracy:', test_acc)
-
-probability_model = tf.keras.Sequential([model,
-                                         tf.keras.layers.Softmax()])
-
-predictions = probability_model.predict(test_images)
-
-print(np.argmax(predictions[0]))
-print(test_labels[0])
 """
 
-print("Hello World!")
+#model = tf.keras.Sequential()
+
+# Okay! This uh...creates tuple of integers.
+# Specifically, that it will be an inpu t
+x = tf.keras.Input(shape=(32,))
+y = tf.keras.layers.Dense(16, activation='softmax')(x)
+
+
+print ("Hello World!")
